c7e84d99e10abf87505e10fadb868559c06008d9
Katie Hanss <KatieHanss@nat-oitwireless-inside-vapornet100-b-16663.Princeton.EDU>
2017-03-07 10:00:22 -0500

    added Tags model to admin

--- athletes/admin.py
+++ athletes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Activity 
+from .models import Activity, Tag
 
 # Register your models here.
 admin.site.register(Activity)
+admin.site.register(Tag)
